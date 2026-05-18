0d47f87f64bdf18c048621a17f1fb0fb62d183fd
VaishnaviGund42 <vaishnavigund42@gmail.com>
2026-05-18 22:43:41 +0530

    to created the apis

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const connectDB = require('./config/db');
+const { connectRedis } = require('./config/redis');
+const feedRoutes = require('./routes/feedRoutes');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.set('io', io);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+
+app.use('/feed', feedRoutes);
+
+app.get('/', (req, res) => {
+  res.send('SyncUp Backend Running');
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+const startServer = async () => {
+  await connectDB();
+  await connectRedis();
+
+  const PORT = process.env.PORT || 5000;
+
+  server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
